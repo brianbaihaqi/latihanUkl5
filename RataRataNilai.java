480a926deb7bb9a7e7225ee226b0e023fd728a50
brianbaihaqi <iffat_baihaqi_34rpl@student.smktelkom-mlg.sch.id>
2025-11-04 10:12:25 +0700

    Add files via upload

---
+++ RataRataNilai.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class RataRataNilai {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah siswa: ");
+        int jumlahSiswa = input.nextInt();
+
+        double totalNilai = 0;
+
+        for (int i = 1; i <= jumlahSiswa; i++) {
+            System.out.print("Masukkan nilai siswa ke-" + i + ": ");
+            double nilai = input.nextDouble();
+            totalNilai += nilai;
+        }
+
+        double rataRata = totalNilai / jumlahSiswa;
+        System.out.println("Nilai rata-rata ujian adalah: " + rataRata);
+
+        input.close();
+    }
+}
